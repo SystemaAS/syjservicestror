a8ebfdae94d7a2c2cace9549d98e24b8d2ed6028
Oscar de la Torre <oscar@systema.no>
2019-02-06 13:35:00 +0100

    Update - Controller PARF

---
+++ src/no/systema/jservices/tror/controller/TrorResponseOutputterController_PARF.java
@@ -0,0 +1,118 @@
+package no.systema.jservices.tror.controller;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import no.systema.jservices.common.dao.ParfDao;
+import no.systema.jservices.common.dao.services.BridfDaoService;
+import no.systema.jservices.common.dao.services.ParfDaoService;
+import no.systema.jservices.common.dto.HeadfDto;
+import no.systema.jservices.common.json.JsonResponseWriter2;
+import no.systema.jservices.common.json.JsonResponseWriter;
+import no.systema.jservices.common.util.ApplicationPropertiesUtil;
+import no.systema.jservices.common.util.CSVOutputter;
+import no.systema.jservices.common.util.StringUtils;
+
+@Controller
+public class TrorResponseOutputterController_PARF {
+	private static final Logger logger = Logger.getLogger(TrorResponseOutputterController_PARF.class.getName());
+	
+	/**
+	 * File: 	PARF - Flyfraktbrev export - Tradevision Participant(flygbolag) file table
+	 * 
+	 * @Example SELECT list http://localhost:8080/syjservicestror/syjsPARF.do?user=OSCAR&parid=XXX
+	 * 
+	 * 
+	 * 
+	 */
+	@RequestMapping(value="syjsPARF.do", method={RequestMethod.GET, RequestMethod.POST})
+	@ResponseBody
+	public String syjsSELECT(HttpSession session, HttpServletRequest request) {
+		JsonResponseWriter2<ParfDao> jsonWriter = new JsonResponseWriter2<ParfDao>();
+		StringBuffer sb = new StringBuffer();
+		List<ParfDao> daoList = new ArrayList<ParfDao>();
+		String user = request.getParameter("user");
+		
+		try {
+			logger.info("Inside syjsPARF.do");		
+			
+			// Check ALWAYS user in BRIDF
+			String userName = bridfDaoService.getUserName(user); 
+			String errMsg = "";
+			String status = "ok";
+			StringBuffer dbErrorStackTrace = new StringBuffer();
+
+			if (StringUtils.hasValue(userName)) {
+				ParfDao dao = new ParfDao();
+				ServletRequestDataBinder binder = new ServletRequestDataBinder(dao);
+				binder.bind(request);
+				//get list
+				if(StringUtils.hasValue(dao.getPaid())){
+					Map<String, Object> params = new HashMap<String, Object>();
+					params = dao.getKeys();
+					daoList = parfDaoService.findAll(params);
+				}else{
+					daoList = parfDaoService.findAll(null);
+				}
+
+				sb.append(jsonWriter.setJsonResult_Common_GetList(userName, daoList));
+				
+
+			} else {
+				errMsg = "ERROR on SELECT";
+				status = "error";
+				dbErrorStackTrace.append(" request input parameters are invalid: <user>");
+				sb.append(jsonWriter.setJsonSimpleErrorResult(userName, errMsg, status, dbErrorStackTrace));
+			}
+		} catch (Exception e) {
+			logger.info("Error :", e);
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			e.printStackTrace(printWriter);
+			return "ERROR [JsonResponseOutputterController]" + writer.toString();
+		}
+
+		session.invalidate();
+		return sb.toString();
+
+	}
+
+	
+	
+
+	@Qualifier ("bridfDaoService")
+	private BridfDaoService bridfDaoService;
+	@Autowired
+	@Required
+	public void setBridfDaoService (BridfDaoService value){ this.bridfDaoService = value; }
+	public BridfDaoService getBridfDaoService(){ return this.bridfDaoService; }	
+
+	
+	@Qualifier ("parfDaoService")
+	private ParfDaoService parfDaoService;
+	@Autowired
+	@Required
+	public void setParfDaoService(ParfDaoService value){ this.parfDaoService = value; }
+	public ParfDaoService getParfDaoService(){ return this.parfDaoService; }		
+	
+	
+}
